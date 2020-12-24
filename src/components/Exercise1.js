5432ea6df3264ed5f9ddecb4479b2af6c0190b49
Shorouq Fakhoury <Shorouq.fakhoury@gmail.com>
2020-12-24 10:37:41 +0200

    exercises

--- src/components/Exercise1.js
+++ src/components/Exercise1.js
@@ -9,13 +9,20 @@ class Exercise1 extends Component {
             age: ""
         }
     }
-
+    change=(event)=>{
+        const name = event.target.name
+        const value = event.target.value
+        this.setState({[name]: value})
+    }
+    clicked=()=>{
+        alert(`Come in ${this.state.name}, you're ${this.state.age} - that's good enough`)
+    }
     render() {
         return (
             <div>
-                <input id="name-input" />
-                <input id="age-input" />
-                <button>Go to Bar</button>
+                <input id="name-input" name="name" onChange={this.change} value={this.state.name}/>
+                <input id="age-input" name="age" onChange={this.change} value={this.state.age}/>
+                <button onClick={this.clicked}>Go to Bar</button>
             </div>
         );
     }
--- src/components/Exercise2.js
+++ src/components/Exercise2.js
@@ -9,12 +9,25 @@ constructor() {
         fruit: ""
     }
 }
-
+change=(event)=>{
+    if(event.target.name === "name"){
+        this.setState({name: event.target.value})
+    }else{
+        this.setState({fruit: event.target.value}, ()=>{
+            console.log(`${this.state.name} selected ${this.state.fruit}`);
+        })
+        
+    }
+}
 render() {
     return (
         <div>
-            <input id="name-input" />
-            <select id="select-input"></select>
+            <input id="name-input" value={this.state.name} onChange={this.change} name="name" />
+            <select id="select-input" onChange={this.change} name="fruit">
+                <option value="apple">apple</option>
+                <option value="banana">banana</option>
+                <option value="orange">orange</option>
+            </select>
         </div>
     );
 }
